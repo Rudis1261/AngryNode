cc7c72c8d19cc384a8ea0ca720d7e3afe877ce92
Rudi Strydom <iam@thatguy.co.za>
2017-02-23 20:39:23 +0000

    Workign Firebase Example

--- client-angular-io/src/app/app.module.ts
+++ client-angular-io/src/app/app.module.ts
@@ -4,8 +4,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
+// Libs
+import { FIREBASE_PROVIDERS, AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
+// Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyAGz8N7LXf7-gDatSoI_a6NENGSml1P7-c",
+  authDomain: "bounce-2a976.firebaseapp.com",
+  databaseURL: "https://bounce-2a976.firebaseio.com",
+  storageBucket: "bounce-2a976.appspot.com",
+  messagingSenderId: "853796948041"
+};
+
+export const firebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+};
 
 @NgModule({
   declarations: [
@@ -16,9 +34,10 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ FIREBASE_PROVIDERS ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- client-angular-io/src/app/home/home.component.ts
+++ client-angular-io/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularFire, FirebaseRef, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  
+  messages: FirebaseListObservable<any[]>;
+  newMessage: Object;
+  
+  constructor(public af: AngularFire) {
+    this.messages = af.database.list('/messages/');
+    this.newMessage = {
+      "name": "",
+      "text": "",
+      "date": ""
+    };
+  }
+  
+  saved() {
+    console.log('item added') ;
+    this.newMessage["text"] = "";
+  }
+  
+  onSubmit(form) { 
+    if (this.newMessage["name"] !== "" && this.newMessage["text"] !== "") {
+      this.newMessage["date"] = new Date().getTime();
+      console.log(this.newMessage);
+      this.messages.push(this.newMessage).then(
+        () => this.saved(),
+        console.error
+      );
+    }
+  }
+  
+  keyDownFunction(event, form) {
+    if(event.keyCode == 13) {
+      this.onSubmit(form);
+    }
   }
 
+  ngOnInit() {}
 }
